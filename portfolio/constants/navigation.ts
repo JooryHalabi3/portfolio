e4fd8e9016a099cc74a6b22af23aadd631c0aa23
Joory Halabi <Joory6067@hotmail.com>
2026-07-28 14:58:29 +0300

    Update navigation.ts

--- portfolio/constants/navigation.ts
+++ portfolio/constants/navigation.ts
@@ -24,20 +24,3 @@ export const navigation = [
     href: "#contact",
   },
 ];
-
-export const SITE = {
-  name: "Joory Halabi",
-
-  title: "Software Engineer",
-
-  email: "Joory6067@hotmail.com",
-
-  location: "Saudi Arabia",
-
-  github: "https://github.com/JooryHalabi3",
-
-  linkedin:
-    "https://www.linkedin.com/in/joory-halabi-978661366",
-
-  resume: "/resume.pdf",
-};
